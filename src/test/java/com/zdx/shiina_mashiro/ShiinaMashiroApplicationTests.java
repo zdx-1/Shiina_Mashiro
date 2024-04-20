6b744c004032a4d638f882e1fecc34f5c8ba2aed
zdx-1 <19546328912@163.com>
2024-04-20 09:37:55 +0800

    创建项目

---
+++ src/test/java/com/zdx/shiina_mashiro/ShiinaMashiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdx.shiina_mashiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiinaMashiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
